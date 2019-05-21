1b29eef1cce0a8cecb60b938771ab53ccb5a19a7
Pablo <budaya.anim@gmail.com>
2019-05-21 20:29:40 +0200

    Creado primer modelo en models.py y ejecutados migrate y makemigrations

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    titulo = models.CharField(max_length=70)
+    short_description = models.CharField(max_length=150)
+    content = models.TextField()
+    image = models.URLField(null=True, blank=True)
+    video = models.URLField(null=True, blank=True)
+    publish_Date = models.DateTimeField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    modification_date = models.DateTimeField(auto_now=True)
