98fbb61f46dffb2a684a194015d7b0dd5f65c618
Pablo <budaya.anim@gmail.com>
2019-05-22 20:50:21 +0200

    Creado filtro por fecha en views.py inferior a today post futuros no se muestran

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
@@ -6,7 +8,7 @@ from posts.models import Post
 
 def latest_posts(request):
 
-    posts = Post.objects.all().order_by('-modification_date')
+    posts = Post.objects.all().filter(publish_Date__lte=datetime.datetime.now()).order_by('-modification_date')
     context = {'posts': posts[:4]}
     html = render(request, 'posts/latest.html', context)
 
